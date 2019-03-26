43a4d8b207f68d1c81e77dc755bcc331ef026e5e
Angela Mumba <malaika.mumba@gmail.com>
2019-03-26 06:53:47 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+ return  num1/num2
 end
 
-def assign_variable(value)
+def assign_variable(name="Killmonger")
+   "#{name}"
   
 end
 
-def argue
-  
+def argue(phrase)
+
+   "#{phrase}"
 end
 
-def greeting
-
+def greeting(greeting="Welcome",name="Salome")
+   "#{greeting},#{name}"
 end
 
 def return_a_value
-
+  puts "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value (last="expert")
+  puts " #{last}"
 end
 
-def pizza_party
+def pizza_party(pizza="cheese")
+puts "#{pizza}"
 
 end
